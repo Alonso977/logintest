3fad9185fa4533c0265dcc49a14c31c758507795
Alonso977 <105885596+Alonso977@users.noreply.github.com>
2023-05-13 10:33:09 -0400

    Initial Commit

---
+++ meal/mealApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealApp.swift
+//  meal
+//
+//  Created by Alonso Alas on 13/5/23.
+//
+
+import SwiftUI
+
+@main
+struct mealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
