d7c2f561e8913ffb388e72e48624b081631cf5f3
Alonso Alas <alonsoalas@Alonsos-MBP.attlocal.net>
2023-05-26 14:04:58 -0700

    Adding new functions

---
+++ LoginApp/LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Alonso Alas on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
